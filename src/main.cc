22d3a1aea038f7e711dc14415a98c1b6fe9654bc
Hasan Eroglu <hasanheroglu@gmail.com>
2023-06-06 10:59:21 +0200

    add context aware support

--- src/main.cc
+++ src/main.cc
@@ -5,4 +5,8 @@ NAN_MODULE_INIT(InitModule) {
     MbusMaster::Init(target);
 }
 
+#if NODE_MAJOR_VERSION >= 10
+NAN_MODULE_WORKER_ENABLED(mbus, InitModule)
+#else
 NODE_MODULE(mbus, InitModule)
+#endif
